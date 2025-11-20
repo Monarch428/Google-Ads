b4d4f2898ad2ddebcdd14b2ea0460da4999d4a62
Satheshkumar <skumar.v.a1999@gmail.com>
2025-11-20 20:13:13 +0530

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@
   import path from 'path';
 
   export default defineConfig({
-    root: 'frontend',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
